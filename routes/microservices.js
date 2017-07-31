1a4e0b0604da45a90a34051c248046341deacd08
Mauricio <mauricio.cadavid@yuxiglobal.com>
2017-07-31 14:24:40 -0500

    updating service

--- routes/microservices.js
+++ routes/microservices.js
@@ -2,5 +2,5 @@ var express = require('express');
 var router = express.Router();
 var MicroCtrl = require('../controllers/microservices');
 
-router.post('/', MicroCtrl.turnOnOffLed);
+router.post('/turnOnOffLed', MicroCtrl.turnOnOffLed);
 module.exports = router;

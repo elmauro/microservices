a1df92c03ead6cc877f20f41ccae3db35bf7adf3
Mauricio <mauricio.cadavid@yuxiglobal.com>
2017-08-02 17:41:55 -0500

    updating services

--- controllers/microservices.js
+++ controllers/microservices.js
@@ -1,18 +1,14 @@
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://10.3.8.212:1883');
 
+client.subscribe('LED');
+
 exports.turnOnLed = function(req, res){
-	client.subscribe('LED');
-
 	client.publish('LED', 'turnOnLed');
-
 	res.status(200).json({'message': 'turnOnLed'});
 }
 
 exports.turnOffLed = function(req, res){
-	client.subscribe('LED');
-
 	client.publish('LED', 'turnOffLed');
-
 	res.status(200).json({'message': 'turnOffLed'});
 }
